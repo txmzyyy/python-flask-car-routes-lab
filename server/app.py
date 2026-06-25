7795bf6107f3c638f62a1c55b565554dfa0d00cd
txmzyyy <tamaramk08@gmail.com>
2026-06-25 19:52:38 +0300

    add routing functionallity

--- server/app.py
+++ server/app.py
@@ -1,1 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+@app.route('/')
+def index():
+    return "Welcome Flatiron Cars"
+
+@app.route('/models')
+def car_models():
+    if existing_models:
+        return {"models": existing_models}
+    else:
+        return {"error": "No such car model exists"}
